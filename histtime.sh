164b441ec5af11a9a46b56039630de044255d88d
Hal Pomeranz <hrpomeranz@gmail.com>
2025-08-07 10:41:47 -0400

    handle tcsh #+<epoch> format

--- histtime.sh
+++ histtime.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while read line; do
-    if [[ "$line" =~ ^# ]]; then
+    if [[ "$line" =~ ^#\+*[0-9][0-9]*$ ]]; then
 	echo -ne $(date -d @$(echo $line | sed 's/#//') '+%F %T')\\t
 	continue
     fi
