04b9364f30374026fd8a19f55b3516eadca2f233
Hal Pomeranz <hrpomeranz@gmail.com>
2026-02-03 14:20:19 -0500

    working around bug in Sys::Utmp

--- wtmpreader.pl
+++ wtmpreader.pl
@@ -1,8 +1,12 @@
 #!/usr/bin/perl
-# Hal Pomeranz (hrpomeranz@gmail.com) - 2025-09-05
+# Hal Pomeranz (hrpomeranz@gmail.com) - 2026-02-02
 
 use POSIX;
 use Sys::Utmp;
+
+# Weirdly Sys::Utmp returns the content from the local /var/log/wtmp file
+# if the file specified on the command line is empty.
+exit(0) if (-z $ARGV[0]);
 
 my $utmp = Sys::Utmp->new('Filename' => $ARGV[0]);
 
