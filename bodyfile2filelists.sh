77152832fbeafef98a674c812da3fc3ed5a37859
Hal Pomeranz <hrpomeranz@gmail.com>
2026-03-11 11:43:45 -0400

    faster version using awk, for inclusion in UAC

--- bodyfile2filelists.sh
+++ bodyfile2filelists.sh
@@ -1,132 +1,91 @@
-#!/bin/bash
-# bodyfile2filelists.sh -- Hal Pomeranz (hrpomeranz@gmail.com)
-# Usage: bodyfile2filelists.sh [-U uacdir] | [-d outputdir] [bodyfile]
+#!/bin/sh
+# shellcheck disable=SC2006
 
 usage() {
     cat <<EOM
 Extract lists of different file and directory types from mactime bodyfile
+during UAC collection.
 
-Usage: $0 [-U uacdir] [-d outputdir] [bodyfile]
+Usage: $0 <directory> [<mountpoint>]
 
-    -U uacdir     Path to UAC directory containing "bodyfile" directory.
-                  Files will be output to the "system" directory here.
-    -d outputdir  Directory where file output goes (default is cwd).
-                  Overrides normal UAC path if "-U" specified.
+<directory> should be a UAC output directory with "bodyfile/bodyfile.txt".
+Output files will be written to the "system" directory.
 
-bodyfile must be specified if no "-U" option. Ignored if "-U".
+Supply <mountpoint> if collecting from directory rather than live system.
 EOM
     exit 1;
 }
 
+# Abort if we can't read bodyfile.txt
+UACDir=$1
+[ -r "$UACDir/bodyfile/bodyfile.txt" ] || usage
+MountDir=$2
 
-OutputDir='.'
-UserOutputDir=
-UACDir=
-CheckUIDs=0
-CheckGIDs=0
-while getopts "d:U:" opts; do
-    case $opts in
-        d) UserOutputDir=$OPTARG
-           ;;
-        U) UACDir=$OPTARG
-           ;;
-        *) usage
-           ;;
-    esac
-done
-shift $(($OPTIND-1))
+BodyFile="$UACDir/bodyfile/bodyfile.txt"
+OutputDir="$UACDir/system"
+mkdir -p "$OutputDir"
 
-if [[ -n "$UACDir" ]]; then
-    BodyFile="$UACDir/bodyfile/bodyfile.txt"
-    if [[ ! -r "$BodyFile" ]]; then
-	echo $BodyFile not found
-	usage
-    fi
-
-    # Put output into appropriate UAC directory
-    OutputDir="$UACDir/system"
-
-    # if UAC, we need to parse /etc/{passwd,group} and get UIDs/GIDs
-    if [[ -r "$UACDir/[root]/etc/passwd" ]]; then
-	declare -A Username
-	IFS=:
-	while read username pwd uid rest; do
-	    Username[$uid]=$username
-	done < "$UACDir/[root]/etc/passwd"
-	unset IFS
-	CheckUIDs=1
-    fi
-    if [[ -r "$UACDir/[root]/etc/group" ]]; then
-	declare -A Groupname
-	IFS=:
-	while read groupname pwd gid rest; do
-	    Groupname[$gid]=$groupname
-	done < "$UACDir/[root]/etc/group"
-	unset IFS
-	CheckGIDs=1
-    fi
-else                               # Not from UAC, give me a bodyfile name
-    BodyFile=$1
-    [[ -r "$BodyFile" ]] || usage
+# Create lists of UIDs and GIDs which will be passed into awk later
+uidlist=
+if [ -r "$MountDir/etc/passwd" ]; then
+    uidlist=`cut -f3 -d: "$MountDir/etc/passwd" | tr '\n' \| | sed 's/|$//'`
 fi
 
-# If user specified output directory with -d then this overrides all
-[[ -n "$UserOutputDir" ]] && OutputDir="$UserOutputDir"
-[[ -d "$OutputDir" ]] || mkdir -p "$OutputDir"
+gidlist=
+if [ -r "$MountDir/etc/passwd" ]; then
+    gidlist=`cut -f3 -d: "$MountDir/etc/group" | tr '\n' \| | sed 's/|$//'`
+fi
 
+# mactime style bodyfile fields are:
 # MD5|name|inode|mode_as_string|UID|GID|size|atime|mtime|ctime|crtime
-IFS=\|
-cat $BodyFile | while read md5 fpath inode fmode uid gid size atime mtime ctime btime; do
 
-    ftype=${fmode:0:1}
-    [[ $ftype == '-' ]] && ftype='f'
-
-    perms=${fmode:1}
-
-    fpath=${fpath/ ->*/}               # remove symlink dests
-    fname=${fpath/*\//}                # like basename
+# Pattern matching in the bodyfile is done in an awk script. For each match,
+# the awk code outputs a filename the match should be written to and the
+# matching path. A shell "while read" loop consumes the awk output and
+# handles actually writing the matches to the appropriate files.
+lastoutfile=
+awk -F\| -v uidlist="$uidlist" -v gidlist="$gidlist" '
+    # Turn the UID and GID lists that were supplied into arrays
+    # indexed by UID and GID for fast lookups
+    BEGIN { split(uidlist, temp)
+            for (i in temp) { uids[temp[i]]=1 }
+	    split(gidlist, temp)
+	    for (i in temp) { gids[temp[i]]=1 }
+    }
 
     # file type "s", socket_files.txt
-    [[ $ftype == 's' ]] && echo $fpath >>"$OutputDir/socket_files.txt"
+    $4 ~ /^s/ {print "socket_files.txt", $2}
     
     # name starts with "." && file type "d", hidden_directories.txt
-    # name starts with "." && file type "f", hidden_files.txt
-    if [[ $fname =~ ^\. ]]; then
-	[[ $ftype == 'd' ]] && echo $fpath >>"$OutputDir/hidden_directories.txt"
-	[[ $ftype == 'f' ]] && echo $fpath >>"$OutputDir/hidden_files.txt"
-    fi
+    $4 ~ /^d/ && $2 ~ /\/\.[^\/]*$/ {print "hidden_directories.txt", $2}
+    # name starts with "." && file type "-", hidden_files.txt
+    $4 ~ /^-/ && $2 ~ /\/\.[^\/]*$/ {print "hidden_files.txt", $2}
 
-    # type "f" && matches "^..[sS]", suid.txt
-    [[ $ftype == 'f' && $perms =~ ^..[sS] ]] && echo $fpath >>"$OutputDir/suid.txt"
-    # type "f" && matches "^.....[sS]", sgid.txt
-    [[ $ftype == 'f' && $perms =~ ^.....[sS] ]] && echo $fpath >>"$OutputDir/sgid.txt"
+    # file type "-" && perms match "^...[sS]", suid.txt
+    $4 ~ /^-..[sS]/ {print "suid.txt", $2}
+    # type "-" && matches "^......[sS]", sgid.txt
+    $4 ~ /^-.....[sS]/ {print "sgid.txt", $2}
 
-    # UAC does one pass for mode 777 files/dirs-- this is wrong
-    # type "f" && matches "^.......w", world_writable_files.txt
-    [[ $ftype == 'f' && $perms =~ ^.......w ]] && echo $fpath >>"$OutputDir/world_writable_files.txt"
-    # type "d" && matches "^.......w", world_writable_directories.txt
-    [[ $ftype == 'd' && $perms =~ ^.......w ]] && echo $fpath >>"$OutputDir/world_writable_directories.txt"
-    # world writable dirs that are not sticky
-    [[ $ftype == 'd' && $perms =~ ^.......w && ! $perms =~ t$ ]] && echo $fpath >>"$OutputDir/world_writable_not_sticky_directories.txt"
-    # type "f" && matches "^....w", group_writable_files.txt
-    [[ $ftype == 'f' && $perms =~ ^....w ]] && echo $fpath >>"$OutputDir/group_writable_files.txt"
-    # type "d" && matches "^....w", group_writable_directories.txt
-    [[ $ftype == 'd' && $perms =~ ^....w ]] && echo $fpath >>"$OutputDir/group_writable_directories.txt"
+    # World writable files and directories
+    $4 ~ /^-.......w/ {print "world_writable_files.txt", $2}
+    $4 ~ /^d.......w/ {print "world_writable_directories.txt", $2}
+    $4 ~ /^d.......w[^t]/ {print "world_writable_not_sticky_directories.txt", $2}
 
-    # for the next two, we need to have parsed /etc/{passwd,group} above
-    if [[ $CheckUIDs -gt 0 ]]; then
-	# user_name_unknown_files.txt
-	[[ $ftype == 'f' && -z "${Username[$uid]}" ]] && echo $fpath >>"$OutputDir/user_name_unknown_files.txt"
-	# user_name_unknown_directories.txt
-	[[ $ftype == 'd' && -z "${Username[$uid]}" ]] && echo $fpath >>"$OutputDir/user_name_unknown_directories.txt"
-    fi
-    
-    if [[ $CheckGIDs -gt 0 ]]; then
-	# group_name_unknown_files.txt
-	[[ $ftype == 'f' && -z "${Groupname[$gid]}" ]] && echo $fpath >>"$OutputDir/group_name_unknown_files.txt"
-	# group_name_unknown_directories.txt
-	[[ $ftype == 'd' && -z "${Groupname[$gid]}" ]] && echo $fpath >>"$OutputDir/group_name_unknown_directories.txt"
-    fi
-done
-unset IFS
+    # Group writable files and directories
+    $4 ~ /^-....w/ {print "group_writable_files.txt", $2}
+    $4 ~ /^d....w/ {print "group_writable_directories.txt", $2}
 
+    # Unknown user/group files and directories
+    length(uidlist) && !($5 in uids) && $4 ~ /^-/ {print "user_name_unknown_files.txt", $2}
+    length(uidlist) && !($5 in uids) && $4 ~ /^d/ {print "user_name_unknown_directories.txt", $2}
+    length(gidlist) && !($6 in gids) && $4 ~ /^-/ {print "group_name_unknown_files.txt", $2}
+    length(gidlist) && !($6 in gids) && $4 ~ /^d/ {print "group_name_unknown_directories.txt", $2}' "$BodyFile" |
+    sort | while read -r outfile filepath; do
+               # Input lines are sorted by the output file, so just
+               # switch the output destination when that file name changes
+               if [ "$outfile" != "$lastoutfile" ]; then
+		   exec >"$OutputDir/$outfile"
+		   lastoutfile="$outfile"
+	       fi
+	       echo "$filepath"
+           done
